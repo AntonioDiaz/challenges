59502f0877d994388cd98a8dcf3021887c2989a0
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2012-11-28 13:10:28 +0100

    resolutión of the algorithm.

---
+++ 02_criba_Erastotenes/src/CribaErastotenes.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ * http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+ * @author antonio.diaz.arroyo
+ *
+ */
+
+public class CribaErastotenes {
+
+	/**
+	 * Calculate the primes with the Eratosthenes Algorithm 
+	 * @param n
+	 * @return
+	 */
+	public static Set<Integer> primes(Integer n){
+		Set<Integer> primes = new HashSet<Integer>();
+		Set<Integer> markedItems = new HashSet<Integer>();
+		for (int i = 2; i <= n; i++) {
+			if (!markedItems.contains(i)){
+				primes.add(i);
+				/* mark all multiples. */
+				int j = 1;
+				while (i*j<=n) {
+					markedItems.add(i*j);
+					j++;
+				}
+			}
+		}
+		/*
+		Integer[] myArray = (Integer[])primes.toArray(new Integer[0]);
+		Arrays.sort(myArray);
+		System.out.println(Arrays.toString(myArray));
+		*/
+		return primes;
+	}
+	
+}
---
+++ 02_criba_Erastotenes/src/CribaErastotenesTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.junit.Test;
+
+
+public class CribaErastotenesTest {
+
+	@Test
+	public void testPrimes() {
+		Integer[] primesOf2 = new Integer[]{2};
+		Integer[] primesOf3 = (Integer[])ArrayUtils.addAll(primesOf2, new Integer[]{3});
+		Integer[] primesOf30 = (Integer[])ArrayUtils.addAll(primesOf3, new Integer[]{ 5, 7, 11, 13, 17, 19, 23, 29});
+		Integer[] primesOf120 = (Integer[])ArrayUtils.addAll(primesOf30, new Integer[]
+				{ 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113});
+		Set<Integer> set2 = new HashSet<Integer>(Arrays.asList(primesOf2));
+		Set<Integer> set3 = new HashSet<Integer>(Arrays.asList(primesOf3));
+		Set<Integer> set30 = new HashSet<Integer>(Arrays.asList(primesOf30));
+		Set<Integer> set120 = new HashSet<Integer>(Arrays.asList(primesOf120));
+		assertTrue(CribaErastotenes.primes(2).equals(set2));
+		assertTrue(CribaErastotenes.primes(3).equals(set3));
+		assertTrue(CribaErastotenes.primes(30).equals(set30));
+		assertTrue(CribaErastotenes.primes(120).equals(set120));
+	}
+}
